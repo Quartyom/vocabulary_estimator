ccd0af7659771728f2319761cf21aec5e5e9de26
Artyom <quartyom@gmail.com>
2022-09-06 13:06:32 +0300

    v1.1

--- dictionary tester.py
+++ dictionary tester.py
@@ -2,16 +2,29 @@ import random
 
 while True:
     print("type 0 for simple mode and 1 for a hard one")
+    print("or you may input your own list of words by dragging your file here")
     inp = input(">>> ")
     if inp == "0":
         f = open("vocabulary.txt", "r")
         how_much_to_show = 75
         break
-    if inp == "1":
+    elif inp == "1":
         f = open("test.txt", "r")
         how_much_to_show = 100
         break
-    
+    else:
+        try:
+            f = open(inp, "r")
+            how_much_to_show = 100
+        except:
+            print("Can't open a file:", inp)
+            continue
+        try:
+            how_much_to_show = int(input("How many words to show? "))
+        except:
+            print("Something went wrong")
+        break
+            
 
 work_list = f.readlines()
 
